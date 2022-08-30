e85336307624bb9899ee4562c439f6e2245729df
winston kiprop <winstonekip50@gmail.com>
2022-08-30 10:16:53 +0300

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,10 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+end
+a = Artist.create(name: 'Jon')
+a.age=30
+a.save
+
+artist=Artist.create(name: 'Kelly')
+artist.age=26
+artist.save
